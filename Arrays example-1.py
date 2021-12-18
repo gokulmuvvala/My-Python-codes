9c3c1850efef56ed05cbbe8965010a345e5da5d7
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-12-18 09:44:15 +0530

    Update Arrays example-1.py

--- Arrays example-1.py
+++ Arrays example-1.py
@@ -2,5 +2,5 @@ import array as myarray  #array name is myarray
 a = myarray.array('i',[200, 4, 92, 87]) # i represents signed int
 
 print("Array 1st element is:",a[0])
-print("Array last element is:",a[-1])
+print("Array last element is:",a[-1]) # here a[-1] is 87
 print(a) # printing entire 
