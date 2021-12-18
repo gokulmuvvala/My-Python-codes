281e50a09af3cdbfee707312023b409936ad8a65
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-12-18 11:14:10 +0530

    Update array example-6.py

--- array example-6.py
+++ array example-6.py
@@ -9,18 +9,18 @@ print("")
 # Adding 2 array arr1 + arr2 
 arr3 = arr1 + arr2
 print(arr3)
-print("")
+print("") # This for creating Gap 
 print(sin(arr3)) # finding the sin values of arr3
-print("")
+print("") # This for creating Gap 
 print(log(arr3)) # finding the log values of arr3
-print("")
+print("") # This for creating Gap 
 print(sqrt(arr3)) # finding the square root of values of arr3
-print("")
+print("") # This for creating Gap 
 print(sum(arr3)) # finding the sum values of arr3
-print("")
+print("") # This for creating Gap 
 print(max(arr3)) # finding the max value of arr3
-print("")
+print("") # This for creating Gap 
 print(min(arr3)) # finding the min value of arr3
-print("")
+print("") # This for creating Gap 
 print(concatenate([arr1,arr2])) # mergeing of 2 arrays
  
